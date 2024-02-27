b6909b1ae23a2a503ae022f4efbc5d72b61fe591
Teja <jagutejaswi94@gmail.com>
2024-02-27 16:46:09 -0600

    pushing the demo2 to github

---
+++ eclipse-workspace/abcnetbankingGit/src/main/java/gitabcnetbank/demo2.java
@@ -0,0 +1,11 @@
+package gitabcnetbank;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("trying to  commit and  push the project into github ");
+
+	}
+
+}
