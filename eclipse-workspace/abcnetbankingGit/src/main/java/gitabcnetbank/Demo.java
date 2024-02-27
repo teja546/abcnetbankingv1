8e308248cf8ee82b6c57f75351e38a972e330567
Teja <jagutejaswi94@gmail.com>
2024-02-27 16:41:05 -0600

    first project commiting to git

---
+++ eclipse-workspace/abcnetbankingGit/src/main/java/gitabcnetbank/Demo.java
@@ -0,0 +1,11 @@
+package gitabcnetbank;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("trying to push the project into github ");
+
+	}
+
+}
