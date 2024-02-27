bdf9d86f9ba97aa621aff5365bec6033a258baf4
Teja <jagutejaswi94@gmail.com>
2024-02-27 17:30:26 -0600

    pushing the maven-demo project to github

---
+++ maven-demo/src/main/java/MAVEN_HOME/demos/App.java
@@ -0,0 +1,13 @@
+package MAVEN_HOME.demos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
